12aa40b790c40ce6629520ab93f69f1d6fb4df08
sushimat <sushimat@fit.cvut.cz>
2025-02-14 23:50:43 +0100

    Remove useless code. Incapsulate bag items into separate class

--- Сумка богини Флакс/Form1.Designer.cs
+++ Сумка богини Флакс/Form1.Designer.cs
@@ -38,20 +38,20 @@
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(34, 18);
+            this.label1.Location = new System.Drawing.Point(26, 15);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(324, 29);
+            this.label1.Size = new System.Drawing.Size(243, 24);
             this.label1.TabIndex = 0;
             this.label1.Text = "Введите число от 1 до 100 :";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // textBox_main
             // 
-            this.textBox_main.Location = new System.Drawing.Point(364, 18);
+            this.textBox_main.Location = new System.Drawing.Point(273, 15);
+            this.textBox_main.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox_main.Name = "textBox_main";
-            this.textBox_main.Size = new System.Drawing.Size(76, 22);
+            this.textBox_main.Size = new System.Drawing.Size(58, 20);
             this.textBox_main.TabIndex = 1;
-            this.textBox_main.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // btn_raschet
             // 
@@ -59,13 +59,14 @@
             this.btn_raschet.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.btn_raschet.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.btn_raschet.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(0)))), ((int)(((byte)(187)))));
-            this.btn_raschet.Location = new System.Drawing.Point(29, 71);
+            this.btn_raschet.Location = new System.Drawing.Point(22, 58);
+            this.btn_raschet.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_raschet.Name = "btn_raschet";
-            this.btn_raschet.Size = new System.Drawing.Size(273, 87);
+            this.btn_raschet.Size = new System.Drawing.Size(205, 71);
             this.btn_raschet.TabIndex = 2;
             this.btn_raschet.Text = "Открыть сумку!";
             this.btn_raschet.UseVisualStyleBackColor = false;
-            this.btn_raschet.Click += new System.EventHandler(this.btn_raschet_Click);
+            this.btn_raschet.Click += new System.EventHandler(this.btn_OpenBagClick);
             // 
             // label2
             // 
@@ -74,27 +75,27 @@
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 84F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(240)))), ((int)(((byte)(48)))));
             this.label2.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.label2.Location = new System.Drawing.Point(28, 177);
+            this.label2.Location = new System.Drawing.Point(21, 144);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(731, 199);
+            this.label2.Size = new System.Drawing.Size(548, 162);
             this.label2.TabIndex = 3;
             this.label2.Text = "?";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(826, 450);
+            this.ClientSize = new System.Drawing.Size(620, 366);
             this.Controls.Add(this.btn_raschet);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox_main);
             this.Controls.Add(this.label1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Сумка Богини Флакс";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
